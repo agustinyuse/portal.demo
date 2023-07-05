a26b01ebe340cf8ce1147435b394955a7ac3ab71
agustinyuse <agustinyuse@gmail.com>
2023-07-05 18:09:08 -0300

    Update BaseService.tsx

--- src/BaseService.tsx
+++ src/BaseService.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://demochatgpt.onrender.com",
+  baseURL: "https://demochatgpt.onrender.com"
 });
 
 export const getAll = () => {
